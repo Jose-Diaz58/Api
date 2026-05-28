777db385c579ef92068745ec676d5f34d7234d22
Jose Diaz <joseluisdiaztrinidad2@gmail.com>
2026-05-28 16:18:15 -0600

    Conexion con atlas, ahora si

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const mongoose = require("mongoose");
 const app = require ("./app");
-
 const port = process.env.PORT || 4000;
 
-const URI_NUBE = "mongodb+srv://BL4Z3:admintaco123@cluster0.qnafqwo.mongodb.netAdminTaco/?";
+const URI_NUBE = "mongodb+srv://BL4Z3:JOSEdiaz4@cluster0.qnafqwo.mongodb.net/AdminTaco";
 
-const conexionDB = process.env.MONGO_URI || URI_NUBE;
-
-mongoose.connect(conexionDB)
+mongoose.connect(URI_NUBE)
 .then(() => console.log("¡Conectado a MongoDB Atlas en la nube! 🚀"))
 .catch(error => console.log("Error de conexión:", error));
 
-// Aqui escucha al puerto el server de express
 app.listen(port, ()=>{
     console.log("********************************")
     console.log("****En efecto, se ejecuta*******")
